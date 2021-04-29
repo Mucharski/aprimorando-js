44c1fc85669d97c118f563bbcdcb0862f5c0e323
Igor <igor.m99@hotmail.com>
2021-04-29 19:08:32 -0300

    projeto quase finalizado

--- Churrascômetro/index.js
+++ Churrascômetro/index.js
@@ -1,20 +1,63 @@
-let imagens = document.getElementsByClassName("selecionar-imagem");
-for (let i in imagens) {
-    imagens[i].addEventListener("click", () => {
-        adicionarActive(i);
+import { Man, Woman, Kid } from "./models/People.js"
+
+let quantidadeHomem = document.getElementById("homens");
+let quantidadeMulher = document.getElementById("mulheres");
+let quantidadeCrianca = document.getElementById("criancas");
+let duracao = document.getElementById("duracao");
+
+const buttonCalc = document.getElementById("btnCalcular");
+
+buttonCalc.addEventListener("click", () => {
+    if (quantidadeHomem.value == "" || quantidadeMulher.value == "" || quantidadeCrianca.value == "" || duracao.value == "") {
+        alert("Preencha todos os campos")
+    } else {
+        let man = new Man(quantidadeHomem.value);
+        let woman = new Woman(quantidadeMulher.value);
+        let kid = new Kid(quantidadeCrianca.value);
+
+        let active = document.getElementsByClassName("active");
+        let guardaAtivos = []
+
+        for (let ac of active) {
+            let elemento = ac.getAttribute("id")
+            guardaAtivos.push(elemento)
+        }
+
+        let qtdPao = (man.quantidade * man.paoDeAlho) + (woman.quantidade * woman.paoDeAlho) + (kid.quantidade * kid.paoDeAlho);
+        let qtdQueijo = (man.quantidade * man.queijoCoalho) + (woman.quantidade * woman.queijoCoalho) + (kid.quantidade * kid.queijoCoalho);
+        let qtdCarne = (man.quantidade * man.grama) + (woman.quantidade * woman.grama) + (kid.quantidade * kid.grama);
+        let qtdFrango = (man.quantidade * man.grama) + (woman.quantidade * woman.grama) + (kid.quantidade * kid.grama);
+        let qtdRefrigerante = (man.quantidade * man.refrigerante) + (woman.quantidade * woman.refrigerante) + (kid.quantidade * kid.refrigerante);
+        let qtdCerveja = (man.quantidade * man.cerveja) + (woman.quantidade * woman.cerveja) + (kid.quantidade * kid.cerveja);
+
+
+        console.log(qtdPao + " " + qtdQueijo + " " + qtdCarne + " " + " " + qtdFrango + " " + qtdRefrigerante + " " + qtdCerveja) // arrumar para não somar
+
+    }
+})
+
+
+let imagens = document.querySelectorAll(".selecionar-imagem");
+
+for (let i of imagens) {
+    i.addEventListener("click", () => {
+        manipularClasse(i);
     })
 }
 
-function adicionarActive(indice) {
-    if (imagens[indice].classList == "selecionar-imagem active") {
+function manipularClasse(indice) {
+    if (indice.classList == "selecionar-imagem active") {
 
-        imagens[indice].classList.remove("active");
-        imagens[indice].classList.add("deactive");
+        indice.classList.remove("active");
+        indice.classList.add("deactive");
 
     } else {
 
-        imagens[indice].classList.add("active");
-        imagens[indice].classList.remove("deactive");
+        indice.classList.add("active");
+        indice.classList.remove("deactive");
 
     }
 }
+
+
+
---
+++ Churrascômetro/models/People.js
@@ -0,0 +1,32 @@
+export class Man {
+    constructor(quantidade) {
+        this.quantidade = quantidade;
+        this.grama = 633;
+        this.cerveja = 3;
+        this.refrigerante = 1;
+        this.paoDeAlho = 2;
+        this.queijoCoalho = 3;
+    }
+}
+
+export class Woman {
+    constructor(quantidade) {
+        this.quantidade = quantidade;
+        this.grama = 478;
+        this.cerveja = 3;
+        this.refrigerante = 1;
+        this.paoDeAlho = 2; 
+        this.queijoCoalho = 2;
+    }
+}
+
+export class Kid {
+    constructor(quantidade) {
+        this.quantidade = quantidade;
+        this.grama = 239;
+        this.cerveja = 0;
+        this.refrigerante = 0.5;
+        this.paoDeAlho = 1.5; 
+        this.queijoCoalho = 2;
+    }
+}
